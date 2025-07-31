e1e7ddd13d31665e14a862e623c5689abd13f6f8
neetapyxeda <neeta.alex@pyxeda.ai>
2025-07-31 12:45:56 +0530

    Removed pip install streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1U1duP9fSw1kw590eHejpApBIwg48QCCm
 """
 
-!pip install -q streamlit
+#!pip install -q streamlit
 
 import streamlit as st
 
